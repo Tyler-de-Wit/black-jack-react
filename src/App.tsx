9c67e90b6ece34ffd59a6a569ffaf60e9c7bf721
Tyler <87571592+Tila05@users.noreply.github.com>
2026-07-12 15:06:47 +0800

    feat: Generate the players and dealers starting hand from a randomised deck

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,67 @@
 import "./App.css";
+import { useState } from "react";
+import shuffleDeck from "./lib/shuffleDeck";
 import PlayingCard from "./components/PlayingCard";
 import deckOfCards from "./data/deckOfCards";
 
+type Card = {
+  id: number;
+  value: string;
+  suit: string;
+};
+
 function App() {
+  const [playersHand, setPlayersHand] = useState<Card[]>([]);
+  const [dealersHand, setDealersHand] = useState<Card[]>([]);
+  const [currentDeck, setCurrentDeck] = useState<Card[]>([]);
+
+  // Initialise a new game using a local fresh deck
+  const startGame = () => {
+    const localDeck = shuffleDeck(deckOfCards);
+
+    // Deal starting cards
+    const pHand = [localDeck[0], localDeck[2]];
+    const dHand = [localDeck[1], localDeck[3]];
+
+    // Remove dealt cards from local deck
+    const remainingDeck = localDeck.slice(4);
+
+    // Update state with local values
+    setPlayersHand(pHand);
+    setDealersHand(dHand);
+    setCurrentDeck(remainingDeck);
+  };
+
+  // Add a card to the player or dealers hand from the current deck
+  const addCardToHand = (
+    setHand: React.Dispatch<React.SetStateAction<Card[]>>,
+  ) => {
+    if (currentDeck.length === 0) return;
+
+    // Draw card from top of deck
+    const drawnCard = currentDeck[0];
+
+    // Update currentDeck and hand state from the drawn card
+    setCurrentDeck((prevDeck) => prevDeck.slice(1));
+    setHand((prevHand) => [...prevHand, drawnCard]);
+  };
+
   return (
-    <PlayingCard value={deckOfCards[28].value} suit={deckOfCards[28].suit} />
+    <>
+      <div>
+        <h2>Dealers Hand</h2>
+        {dealersHand.map((card) => (
+          <PlayingCard value={card.value} suit={card.suit} key={card.id} />
+        ))}
+      </div>
+      <div>
+        <h2>Players Hand</h2>
+        {playersHand.map((card) => (
+          <PlayingCard value={card.value} suit={card.suit} key={card.id} />
+        ))}
+      </div>
+      <button onClick={startGame}>New Game</button>
+    </>
   );
 }
 
---
+++ src/lib/shuffleDeck.ts
@@ -0,0 +1,17 @@
+type Card = {
+  id: number;
+  value: string;
+  suit: string;
+};
+
+// Use Fisher-Yates Shuffle to randomise deck of cards
+const shuffleDeck = (array: Card[]) => {
+  const clonedArray = [...array];
+  for (let i = clonedArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [clonedArray[i], clonedArray[j]] = [clonedArray[j], clonedArray[i]];
+  }
+  return clonedArray;
+};
+
+export default shuffleDeck;
