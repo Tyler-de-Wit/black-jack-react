17f3260a495b941b396cdf258113a0f1b95a86f7
Tyler <87571592+Tila05@users.noreply.github.com>
2026-07-12 13:23:15 +0800

    feat: Add the data for a full deck of cards

---
+++ src/data/deckOfCards.ts
@@ -0,0 +1,69 @@
+type Card = {
+  id: number;
+  value: string;
+  suit: string;
+};
+
+const deckOfCards: Card[] = [
+  // Hearts
+  { id: 1, value: "Ace", suit: "hearts" },
+  { id: 2, value: "2", suit: "hearts" },
+  { id: 3, value: "3", suit: "hearts" },
+  { id: 4, value: "4", suit: "hearts" },
+  { id: 5, value: "5", suit: "hearts" },
+  { id: 6, value: "6", suit: "hearts" },
+  { id: 7, value: "7", suit: "hearts" },
+  { id: 8, value: "8", suit: "hearts" },
+  { id: 9, value: "9", suit: "hearts" },
+  { id: 10, value: "10", suit: "hearts" },
+  { id: 11, value: "Jack", suit: "hearts" },
+  { id: 12, value: "Queen", suit: "hearts" },
+  { id: 13, value: "King", suit: "hearts" },
+
+  // Diamonds
+  { id: 14, value: "Ace", suit: "diamonds" },
+  { id: 15, value: "2", suit: "diamonds" },
+  { id: 16, value: "3", suit: "diamonds" },
+  { id: 17, value: "4", suit: "diamonds" },
+  { id: 18, value: "5", suit: "diamonds" },
+  { id: 19, value: "6", suit: "diamonds" },
+  { id: 20, value: "7", suit: "diamonds" },
+  { id: 21, value: "8", suit: "diamonds" },
+  { id: 22, value: "9", suit: "diamonds" },
+  { id: 23, value: "10", suit: "diamonds" },
+  { id: 24, value: "Jack", suit: "diamonds" },
+  { id: 25, value: "Queen", suit: "diamonds" },
+  { id: 26, value: "King", suit: "diamonds" },
+
+  // Clubs
+  { id: 27, value: "Ace", suit: "clubs" },
+  { id: 28, value: "2", suit: "clubs" },
+  { id: 29, value: "3", suit: "clubs" },
+  { id: 30, value: "4", suit: "clubs" },
+  { id: 31, value: "5", suit: "clubs" },
+  { id: 32, value: "6", suit: "clubs" },
+  { id: 33, value: "7", suit: "clubs" },
+  { id: 34, value: "8", suit: "clubs" },
+  { id: 35, value: "9", suit: "clubs" },
+  { id: 36, value: "10", suit: "clubs" },
+  { id: 37, value: "Jack", suit: "clubs" },
+  { id: 38, value: "Queen", suit: "clubs" },
+  { id: 39, value: "King", suit: "clubs" },
+
+  // Spades
+  { id: 40, value: "Ace", suit: "spades" },
+  { id: 41, value: "2", suit: "spades" },
+  { id: 42, value: "3", suit: "spades" },
+  { id: 43, value: "4", suit: "spades" },
+  { id: 44, value: "5", suit: "spades" },
+  { id: 45, value: "6", suit: "spades" },
+  { id: 46, value: "7", suit: "spades" },
+  { id: 47, value: "8", suit: "spades" },
+  { id: 48, value: "9", suit: "spades" },
+  { id: 49, value: "10", suit: "spades" },
+  { id: 50, value: "Jack", suit: "spades" },
+  { id: 51, value: "Queen", suit: "spades" },
+  { id: 52, value: "King", suit: "spades" },
+];
+
+export default deckOfCards;
