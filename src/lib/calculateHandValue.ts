57dccaa54acd47ec8d6d042fc4cffa9d65e50b10
Tyler <87571592+Tila05@users.noreply.github.com>
2026-07-14 13:15:16 +0800

    fix: Fix error where aces were being counted as 11 regardless of the cards dealt after them, by moving the aces to the end of the currentHand array to be calculated last

--- src/lib/calculateHandValue.ts
+++ src/lib/calculateHandValue.ts
@@ -1,13 +1,39 @@
 type Card = {
   id: number;
+  // Value of the card: 3, Ace, King etc.
   value: string;
+  // Suit the card belongs to: hearts, diamonds, clubs, spades.
   suit: string;
 };
 
 function calculateHandValue(currentHand: Card[]) {
+  let rearrangedCurrentHand = [...currentHand];
   let currentHandValue = 0;
 
-  currentHand.forEach((card) => {
+  // Function to move ace to end of array as its value needs to be calculated last
+  const moveAceToBack = (arr: Card[], fromIndex: number, toIndex: number) => {
+    const element = arr[fromIndex];
+
+    // Remove ace from currentHand
+    const newArr = arr.toSpliced(fromIndex, 1);
+
+    // Reinsert ace at end of currentHand
+    return newArr.toSpliced(toIndex, 0, element);
+  };
+
+  // Check for any ace's in currend hand and then move them to the back
+  currentHand.forEach((card, index) => {
+    if (card.value === "Ace") {
+      rearrangedCurrentHand = moveAceToBack(
+        rearrangedCurrentHand,
+        index,
+        rearrangedCurrentHand.length - 1,
+      );
+    }
+  });
+
+  // Loop through each card and determine it's value
+  rearrangedCurrentHand.forEach((card) => {
     // Set Jack, Queen or King's value to 10
     if (
       card.value === "Jack" ||
