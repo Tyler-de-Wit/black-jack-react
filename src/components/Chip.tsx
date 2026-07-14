111d283ff6ef1f4c5799f5298ca3839d9ddd61db
Tyler <87571592+Tila05@users.noreply.github.com>
2026-07-14 11:07:49 +0800

    feat: Add SVG's for each chip

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,9 +1,25 @@
+import chip5 from "../assets/chips/chip-5.svg";
+import chip10 from "../assets/chips/chip-10.svg";
+import chip25 from "../assets/chips/chip-25.svg";
+import chip50 from "../assets/chips/chip-50.svg";
+import chip100 from "../assets/chips/chip-100.svg";
+
 type ChipProps = {
   value: number;
   onChipClick: (newBet: number) => void;
 };
 
+const chipSvgs: Record<string, string> = {
+  5: chip5,
+  10: chip10,
+  25: chip25,
+  50: chip50,
+  100: chip100,
+};
+
 function Chip({ value, onChipClick }: ChipProps) {
+  const svgSource = chipSvgs[value];
+
   return (
     <button
       className="chip"
@@ -11,7 +27,7 @@ function Chip({ value, onChipClick }: ChipProps) {
         onChipClick(value);
       }}
     >
-      {value}
+      <img src={svgSource} width={80} height={80} alt={`chip ${value}`} />
     </button>
   );
 }
